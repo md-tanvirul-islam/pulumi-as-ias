7a7ba7fb878b86cfbf6d32abbb0a6cc3766ee86a
Md. Tanvirul Islam <md.tanvirulislam@outlook.com>
2025-05-17 07:19:36 +0600

    vpc tenancy

--- typescript/aws-ts-01/index.ts
+++ typescript/aws-ts-01/index.ts
@@ -5,6 +5,7 @@ import * as awsx from "@pulumi/awsx";
 
 const newVpc = new aws.ec2.Vpc("main", {
     cidrBlock: "10.0.0.0/16",
+    instanceTenancy: "default",
     tags: {
         name: "tan-vpc",
         createBy: "tanvir",
